7602e2f2044c0009dae4ff82f6a49ea34965ec82
Gocchi <centenoilse93@gmail.com>
2017-06-21 17:17:39 +0000

    agregue maquetacion inicio, about me, trabajos y contactame

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
-$(window).on("load click resize", function() {
-  var $black = $("#black").css("clip", "");
-  var w = $(this).width(), h = $(this).height();
-  var t = 0, r = w, b = h, l = 0;
-  TweenLite.set($black, {clip: "rect("+[0,w,h,0].join()+")"});
-  this.tl && tl.stop();
-  tl = new TimelineLite();
-  l = w/2 - 60;
-  tl.to($black, 0.25, {clip: "rect("+[t,r,b,l].join()+")"}, 0.5);
-  b = h/2;
-  tl.to($black, 0.25, {clip: "rect("+[t,r,b,l].join()+")"});
-  r = w/2 + 108;
-  tl.to($black, 0.25, {clip: "rect("+[t,r,b,l].join()+")"});
-  t = h/2 - 60;
-  tl.to($black, 1.75, {clip: "rect("+[t,r,b,l].join()+")", ease: Elastic.easeOut});
-});
+var cargarPagina = function(){
+	$("#btn-1").click(function();
+}
+
+$(document).ready(cargarPagina);
+
+
+
+var origen=document.getElementById('buttonOrigen');
+var textOrigen=document.getElementById('origen');
+
+var evolucion=document.getElementById('buttonEvolucion');
+var textEvolucion=document.getElementById('evolucion');
+
+origen.addEventListener("dblclick", showO);
+origen.addEventListener("click", hiddenO);
+ 
+evolucion.addEventListener("dblclick", showE);
+evolucion.addEventListener("click", hiddenE);
+
+
+function showO() {
+  textOrigen.style.visibility = "visible";
+}
+function hiddenO() {
+  textOrigen.style.visibility = "hidden";
+}
+
+function showE() {
+  textEvolucion.style.visibility = "visible";
+}
+
+function hiddenE() {
+  textEvolucion.style.visibility = "hidden";
+}
+
+var buttonPost=document.getElementById('buttonPost');
+buttonPost.addEventListener("click", postImg);
+
+
+   
